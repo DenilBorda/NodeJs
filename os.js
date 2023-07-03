0b094e092d2a9bbf4b04011854a60eed50ab2703
Denil Borda <denilbordasq@gmail.com>
2023-07-03 18:15:19 +0530

    03-07-23

---
+++ os.js
@@ -0,0 +1,17 @@
+const os = require('os');
+
+// console.log(os.arch());
+// console.log(os.type());
+// console.log(os.networkInterfaces());
+// console.log(os.EOL);
+// console.log(os.hostname());
+// console.log(os.freemem());
+// console.log(os.totalmem());
+// console.log(os.endianness());
+// console.log(os.homedir());
+// console.log(os.userInfo());
+// console.log(os.availableParallelism());
+// console.log(os.cpus());
+// console.log(os.constants);
+// console.log(os.devNull);
+// console.log(os.getPriority());
